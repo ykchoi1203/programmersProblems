574e65eac853438574c342a9c6a6f3ec2c6f3a34
ykchoi1203 <chldudrms94@naver.com>
2022-11-29 08:36:03 +0900

    수정

--- level0/PushString.java
+++ level0/PushString.java
@@ -10,7 +10,7 @@ public class PushString {
         for(int i=0; i<A.length(); i++){
             if(A.equals(B))
                 return i;
-            A = "" + A.charAt(A.length()-1) + A.substring(0, A.length()-1);
+            A = A.charAt(A.length()-1) + A.substring(0, A.length()-1);
         }
 
         return -1;
